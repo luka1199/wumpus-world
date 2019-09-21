b4807a8a130d069d2e411eae6481bb486627c138
Luka Steinbach <luka.steinbach@gmx.de>
2019-09-21 14:09:43 +0200

    Implemented displayGrid()

--- sketch.js
+++ sketch.js
@@ -1,68 +1,113 @@
-let grid = Grid()
+let canvasSize = 500;
+let world;
 
 function setup() {
-    createCanvas(640, 480);
+    createCanvas(canvasSize, canvasSize);
+    world = new World();
 }
 
 function draw() {
     background(255);
-    grid.draw()
+    world.display()
 }
 
-class Grid {
-    constructor () {
-        this.visible =  [[false, false, false, false],
-                        [false, false, false, false],
-                        [false, false, false, false],
-                        [false, false, false, false]]
+class World {
+    constructor() {
+        this.roomSize = canvasSize / 4;
+        this.visible = [
+            [false, false, false, false],
+            [false, false, false, false],
+            [false, false, false, false],
+            [false, false, false, false]
+        ];
+
+        this.agent = new Agent(0, 0);
+        this.showRoom(0, 0);
     }
 
     showRoom(x, y) {
         this.visible[x][y] = true;
     }
 
-    draw() {
+    display() {
+        this.displayGrid();
+        this.displayWorldObjects();
+    }
 
+    displayGrid() {
+        strokeWeight(2);
+        stroke(30);
+        for (var i = 0; i <= 4; i++) {
+            for (var j = 0; j <= 4; j++) {
+                if (this.visible[i][j] == true) {
+                    fill(255);
+                } else {
+                    fill(150);
+                }
+                square(i * this.roomSize, j * this.roomSize, this.roomSize);
+            }
+        }
+
+    }
+
+    displayWorldObjects() {
+        this.agent.display();
     }
 
 }
 
 class Wumpus {
-    constructor (x, y) {
+    constructor(x, y) {
         this.position = createVector(x, y);
     }
 
-    draw() {
+    display() {
 
     }
 }
 
 class Agent {
-    constructor (x, y) {
+    constructor(x, y) {
         this.position = createVector(x, y);
     }
 
-    draw() {
+    display() {
+
+    }
+
+    up() {
+
+    }
+
+    down() {
+
+    }
+
+    left() {
+
+    }
+
+    right() {
 
     }
 }
 
 class Gold {
-    constructor (x, y) {
+    constructor(x, y) {
         this.position = createVector(x, y);
     }
 
-    draw() {
-        
+    display() {
+
     }
 }
 
 class Pit {
-    constructor (x, y) {
+    constructor(x, y) {
         this.position = createCanvas(x, y);
     }
 
-    draw() {
+    display() {
 
     }
 }
