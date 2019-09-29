afefbf212ad82024aa73130eb70f255bb8839e9f
Luka Steinbach <luka.steinbach@gmx.de>
2019-09-29 17:19:32 +0200

    Made progress bar smoother

--- js/progressbar.js
+++ js/progressbar.js
@@ -1,20 +1,27 @@
 class ProgressBar {
     constructor(maxProgress) {
         this.max = maxProgress;
-        this.current = 0;
+        this.progress = 0;
+        this.currentPosition = 0;
+        this.lastChange = -Infinity;
+        this.timer = 0.001;
     }
 
     next() {
-        this.current++;
+        this.progress++;
     }
 
     display() {
         let width = canvas.width * 0.5;
         let height = 10;
-        
+        if (this.currentPosition < this.progress && millis() - this.lastChange > this.timer) {
+            this.currentPosition += 0.5;
+            this.lastChange = millis();
+        }
+
         fill(30);
-        rect(canvas.width / 2 - width / 2, canvas.height / 2 - height, width / this.max * this.current, height);
-        
+        rect(canvas.width / 2 - width / 2, canvas.height / 2 - height, width / this.max * this.currentPosition, height);
+
         strokeWeight(4);
         stroke(30);
         noFill();
