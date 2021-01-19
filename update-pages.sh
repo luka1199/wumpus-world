ab156133c84d86cfa503e67ca98c15d9707d3286
Luka Steinbach <luka.steinbach@gmx.de>
2021-01-19 20:40:10 +0100

    Update pages

--- update-pages.sh
+++ update-pages.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
-
-git add .
-git status
-git commit -m 'Update pages'
-git push origin master
-
 git checkout gh-pages
-git rebase master
+git merge master
+git add .
+git commit -m "Update pages"
 git push origin gh-pages
 git checkout master
