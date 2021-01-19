dd776f9477fd8611412b03e256e59f127dc5e389
Luka Steinbach <luka.steinbach@gmx.de>
2021-01-19 20:29:10 +0100

    Removed focus on slider after mouseup

--- js/misc.js
+++ js/misc.js
@@ -2,3 +2,7 @@ $(document).on('input', '#world-size-slider', function () {
     roomsPerRow = $(this).val();
     restart();
 });
+
+$(document).on('mouseup', '#world-size-slider', function () {
+    $(this).blur();
+});  
